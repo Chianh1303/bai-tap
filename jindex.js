8e2f827b5169b9ca6a0247dcc5b672d76652cef3
chianh <chianhn567@gmail.com>
2023-11-28 11:11:34 +0700

    bai tap

---
+++ jindex.js
@@ -0,0 +1,8 @@
+let  a = +prompt("Nhap a");
+let  b = +prompt("Nhap b");
+let  c = +prompt("Nhap c");
+let  d = +prompt("Nhap d")
+let  sum = a+b+c;
+console. log(sum);
+let avg = sum / 3;
+document.write(avg);
